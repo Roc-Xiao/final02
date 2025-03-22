ea3a40657ae86bd0313fdc62365fa5c908d26672
Roc Xiao <916751792@qq.com>
2025-03-22 19:56:02 +0800

    划分节点，整合功能

---
+++ answer/include/answer/topic_name.h
@@ -0,0 +1,15 @@
+#ifndef TOPIC_NAME_H
+#define TOPIC_NAME_H
+
+namespace topic_name {
+    const char* map = "map_topic";
+    const char* area = "area";
+    const char* robot = "robot";
+    const char* pose = "pose";
+    const char* shoot = "shoot";
+    const char* password = "password";
+    const char* password_segment = "password_segment";
+    const char* image_raw = "image_raw";
+}
+
+#endif //TOPIC_NAME_H
